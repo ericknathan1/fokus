75b8e6adf9ed88cfce97643f9a3cfba816af7d45
William Firmino <prof.williamfirmino@gmail.com>
2025-08-26 09:55:26 -0300

    Subindo a primeira versão

---
+++ app/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>José William</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+
+  }
+})
+
