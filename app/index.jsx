ccc958c1f549d56d199149ec6e57d22977d6fe63
ericknathan1 <erick.nathan.rj@gmail.com>
2025-09-01 10:24:01 -0300

    att 2

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,66 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.font}>Olá mundo</Text>
+      <Image source={require("./foco.png")} style={styles.image}></Image>
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+            Projeto fictício e sem fins comerciais. Desenvolvido por Aluno. 
+        </Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create(
-  {
-    container: {
-      flex:1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "lightblue"
-    },
-    font: {
-      fontSize: 48
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'#021123'
+  },
+  image: {
+    width: 320,
+    height: 320,
+    marginBottom: 40,
+    resizeMode:""
+  },
+  actions: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#0b2b52',
+    borderColor:'#13417a',
+    borderWidth:2,
+    borderRadius: 32,
+    minWidth: 312,
+    maxHeight: 268,
+    padding: 24
+  },
+  timer:{
+    fontSize: 53,
+    fontWeight: 600,
+    color: "white",
+    marginBottom: 32,
+  
+  },
+  button: {
+    width: 264,
+    height: 40,
+    borderRadius: 32,
+    padding: 8,
+    alignItems: "center",
+    backgroundColor: "#B872FF"
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 600
   }
-)
+});
--- documentacao.md
+++ documentacao.md
@@ -1,12 +1,20 @@
 # COMANDOS DO REACT NATIVE
 
-npm start
-Inicia o servidor
-
-npm run reset-project
-Reseta o projeto pra ficar limpo
+1. npx create-expo-app@3 fokus -> criar um app react native com o nome fokus, na versão 3.
+2. npm run reset-project -> reseta o projeto para ficar limpo.
+3. npm start -> prepara o servidor para ser executado.
+4. npm run web -> inicializar o app no navegador.
+5. npm install -> instala todas as dependências necessárias para o projeto rodar.
 
 ## COMPONENTES DO REACT NATIVE
 
-<View></View> -> Funciona como container div. É obrigatório para outros componentes.
-<Text></Text> -> Funciona quando você vai utilizar textos.
+<View></View> -> funciona como container "div". é obrigatório para outros componentes. É por padrão flex.
+<Text></Text> -> funciona quando você vai utilizar textos.
+<Image> -> utilizado para renderizar imagens no projeto.
+<Pressable></Pressable> -> utilizado quando temos algo pressionável.
+
+## FUNÇÕES E CLASSES UTILIZADAS NO REACT NATIVE
+
+1. required() -> faz com que você consiga passar um caminho da img que está dentro do projeto.
+2. create() -> utilizada quando você quer criar algo.
+3. StyleSheet -> utilizado para criar um campo de estilização.
