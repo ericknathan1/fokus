8ad6956444d6f929e0e5e9bc4ca9ac5f7b7daa44
ericknathan1 <erick.nathan.rj@gmail.com>
2025-09-02 10:32:19 -0300

    att 3

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,33 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Index() {
+  const [loaded, error] = useFonts({
+    'Unbounded': require('../assets/fonts/Unbounded.ttf'),
+    'montserrat': require('../assets/fonts/montserrat.ttf')
+  });
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("./foco.png")} style={styles.image}></Image>
       <View style={styles.actions}>
+        <View style={styles.context}>
+          <Pressable style={styles.contextButtonActive}>
+            <Text style={styles.contextButtonText}>Foco</Text>
+          </Pressable>
+          <Pressable style={styles.contextButton}>
+            <Text style={styles.contextButtonText}>Pausa Curta</Text>
+          </Pressable>
+          <Pressable style={styles.contextButton}>
+            <Text style={styles.contextButtonText}>Pausa Longa</Text>
+          </Pressable>
+        </View>
         <Text style={styles.timer}>25:00</Text>
         <Pressable style={styles.button}>
         <Text style={styles.buttonText}>Começar</Text>
@@ -24,13 +47,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:'#021123'
+    backgroundColor:'#021123',
+    gap: 32
   },
   image: {
     width: 320,
     height: 320,
-    marginBottom: 40,
-    resizeMode:""
+    resizeMode:"stretch"
   },
   actions: {
     flex: 1,
@@ -42,13 +65,14 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     minWidth: 312,
     maxHeight: 268,
-    padding: 24
+    padding: 24,
+    gap: 32
   },
   timer:{
-    fontSize: 53,
-    fontWeight: 600,
+    fontSize: 54,
+    fontWeight: 'bold',
     color: "white",
-    marginBottom: 32,
+    fontFamily: 'Unbounded'
   
   },
   button: {
@@ -61,6 +85,43 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 18,
-    fontWeight: 600
+    fontWeight: 'semibold',
+    color:'#021123',
+    fontFamily: 'Unbounded'
+     
+  },
+  footer: {
+    width: '75%'
+  },
+  footerText: {
+    fontFamily:'montserrat',
+    color: '#98A0A8',
+    textAlign: 'center',
+
+  },
+  context: {
+    flex:1,
+    flexDirection:'row'
+  },
+  contextButton: {
+    width:'auto',
+    height:35,
+    justifyContent:'center',
+    borderRadius: 8,
+    padding: 8
+  },
+  contextButtonActive:{
+    width:'auto',
+    height:35,
+    justifyContent:'center',
+    backgroundColor:'#144480',
+    borderRadius: 8,
+    padding: 8
+  },
+  contextButtonText:{
+    fontFamily:'montserrat',
+    fontWeight:'bold',
+    textAlign:'center',
+    color:'white'
   }
 });
